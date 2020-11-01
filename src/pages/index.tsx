ae1e1203aaaebb2a41fdd5a3671c0651c33fbe46
replacem3nts <donny.landis@gmail.com>
2020-10-31 23:51:57 -0400

    changes some copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,13 @@ import { Layout } from "../components/Layout"
 const IndexPage = () => (
   <Layout>
     <div>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
+      <h1>Hi folks!</h1>
+      <p>Welcome to Critter</p>
+      <p>
+        You thought crypograms were hard, try solving crypotgrams that are
+        tweets.
+      </p>
+      <p>On the plus side, you get to choose the twitter account...</p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
       <PuzzleForm
